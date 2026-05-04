b621033df67ceb0a52e80a21b64de8696f1d8a0b
Frank Adcock <frank@bushlife.com.au>
2026-05-04 10:44:30 +1000

    Add description to key vault role key

--- FunctionKeyConfigurator/Services/KeyVaultService.cs
+++ FunctionKeyConfigurator/Services/KeyVaultService.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Azure.Security.KeyVault.Secrets;
 using FunctionKeyConfigurator.Models;
 using Microsoft.Extensions.Logging;
@@ -13,16 +14,25 @@ public class KeyVaultService(ILogger<KeyVaultService> logger, SecretClient secre
         foreach (var roleKey in roleKeys)
         {
             var secretName = $"{roleKey.RoleName}-{environmentSuffix}";
+            var typeValue = SplitCamelCase(roleKey.RoleName) + " Role Function Access Key";
             try
             {
                 logger.LogInformation("Setting secret '{SecretName}' in KeyVault", secretName);
-                await secretClient.SetSecretAsync(secretName, roleKey.KeyValue);
-                logger.LogInformation("Successfully set secret '{SecretName}' in KeyVault", secretName);
+                var secret = new KeyVaultSecret(secretName, roleKey.KeyValue);
+                secret.Properties.ContentType = typeValue;
+                secret.Properties.Tags["Type"] = typeValue;
+                await secretClient.SetSecretAsync(secret);
+                logger.LogInformation("Successfully set secret '{SecretName}' in KeyVault with Type '{Type}'", secretName, typeValue);
             }
             catch (Exception ex)
             {
                 logger.LogError(ex, "Error setting secret '{SecretName}' in KeyVault", secretName);
             }
         }
+    }
+
+    private static string SplitCamelCase(string input)
+    {
+        return Regex.Replace(input, "(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])", " ");
     }
 }
